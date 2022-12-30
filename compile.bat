207e03faf572569197d26ba64f45511b5201ebc4
papum20_lenovo <danieledugo1@gmail.com>
2022-12-30 14:33:02 +0100

    first commit

---
+++ compile.bat
@@ -0,0 +1,17 @@
+::RECURSIVE COMPILER FOR ALL JAVA FILES IN A DIRECTORY AND SUBDIRECTORIES
+
+@echo off
+title MAKE
+
+set argsEmpty=1
+set curPath="src"
+
+for %%x in (%*) do set argsEmpty=0
+IF %argsEmpty%==0 set curPath=%1
+
+FOR /D %%G in ("%curPath%\*") DO (
+	call ./compile %%G
+)
+
+echo %1 %curPath%
+javac -d out -sourcepath src %curPath%\*.java
